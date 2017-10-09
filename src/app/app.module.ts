77df84b58b6dee4c06e968cba22270eafd94c7ce
Mikolaj <mikolajhyzy95@gmail.com>
2017-10-09 19:26:15 +0200

    added the strucutre and routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,25 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { RouterModule }   from '@angular/router';
+import { AppRoutingModule }     from './app-routing.module';
 
 import { AppComponent } from './app.component';
+import { HomeComponent } from './pages/home.component';
+import { AboutComponent } from './pages/about.component';
+import { ContactComponent } from './pages/contact.component';
+import { HeaderComponent } from './shared/header.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    AboutComponent,
+    ContactComponent,
+    HeaderComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
