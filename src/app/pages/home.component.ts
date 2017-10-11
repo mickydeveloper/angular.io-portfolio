60a7aef9da479cb4313ebbc39b95846b1aa0e757
Mikolaj <mikolajhyzy95@gmail.com>
2017-10-11 13:15:42 +0200

    added the typing library

--- src/app/pages/home.component.ts
+++ src/app/pages/home.component.ts
@@ -1,10 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { init, destroy } from 'ityped';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss']
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit {
   title = 'I create to';
+
+  ngOnInit() {
+    init(`#type`, {
+      // required - for now, only accepting texts
+        strings: ['Innovate', 'Enjoy', 'Enrich'],
+        typeSpeed: 100, //default
+        backSpeed: 50, //default
+        startDelay: 500, //default
+        backDelay: 500, //default
+        loop: true, //default
+        showCursor: true, //default
+        cursorChar: '|', //default
+        onFinished: function(){}
+    });
+  }
 }
