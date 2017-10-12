e4dd3ca4b7071be8274b3474d0a6b69bcdc15d8d
Mikolaj <mikolajhyzy95@gmail.com>
2017-10-12 13:54:11 +0200

    added the about me section to the home page

--- src/app/pages/home.component.ts
+++ src/app/pages/home.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { init, destroy } from 'ityped';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-home',
@@ -7,20 +8,18 @@ import { init, destroy } from 'ityped';
   styleUrls: ['./home.component.scss']
 })
 export class HomeComponent implements OnInit {
-  title = 'I create to';
-
   ngOnInit() {
     init(`#type`, {
       // required - for now, only accepting texts
-        strings: ['Innovate', 'Enjoy', 'Enrich'],
-        typeSpeed: 100, //default
-        backSpeed: 50, //default
-        startDelay: 500, //default
-        backDelay: 500, //default
-        loop: true, //default
-        showCursor: true, //default
-        cursorChar: '|', //default
-        onFinished: function(){}
+      strings: ['Innovate', 'Enjoy', 'Enrich'],
+      typeSpeed: 100, //default
+      backSpeed: 50, //default
+      startDelay: 1000, //default
+      backDelay: 1000, //default
+      loop: true, //default
+      showCursor: true, //default
+      cursorChar: '|', //default
+      onFinished: function () { }
     });
   }
 }
